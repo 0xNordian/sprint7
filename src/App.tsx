8300bf550e241355585b804fb5980dece6d2867d
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-09-12 10:12:02 +0200

    totalHandler function refactored

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,30 @@ import './App.css';
 function App() {
   const [total, setTotal] = useState(0);
 
-  const totalHandler = () => {
-    const web = document.querySelector('#web') as HTMLInputElement;
-    const seo = document.querySelector('#seo') as HTMLInputElement;
-    const ads = document.querySelector('#ads') as HTMLInputElement;
-  
-    let total = 0;
+  const totalHandler = (label: string, price: number) => {
+    const service = document.querySelector(label) as HTMLInputElement;
 
-    (web.checked) ? total += 500 : total -= 0;
-    (seo.checked) ? total += 300 : total -= 0;
-    (ads.checked) ? total += 200 : total -= 0;
-
-    setTotal(total);
+    if(service.checked){
+      setTotal((prev) => prev + price);
+    } else {
+      setTotal((prev) => prev - price);
+    }
   }
-
-  console.log("total: ", total);
 
   return (
     <>
       <h2>¿Qué quieres hacer?</h2>
       <div className="services">
         <label>
-          <input type="checkbox" name="web" value="web" id="web" className="exclusive-checkbox" onChange={totalHandler}/>
+          <input type="checkbox" name="web" value="web" id="web" className="exclusive-checkbox" onChange={() => totalHandler("#web", 500)}/>
           Una página web (500€)
         </label>
         <label>
-          <input type="checkbox" name="seo" value="seo" id="seo" className="exclusive-checkbox" onChange={totalHandler}/>
+          <input type="checkbox" name="seo" value="seo" id="seo" className="exclusive-checkbox" onChange={() => totalHandler("#seo", 300)}/>
           Una consultoría SEO (300€)
         </label>
         <label>
-          <input type="checkbox" name="ads" value="ads" id="ads" className="exclusive-checkbox" onChange={totalHandler}/>
+          <input type="checkbox" name="ads" value="ads" id="ads" className="exclusive-checkbox" onChange={() => totalHandler("#ads", 200)}/>
           Una campanya de Google Ads (200€)
         </label>
       <div className="total">Total: {total}</div>
