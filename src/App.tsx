79ccd04a93c8116807b8163c09952211572fadba
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-09-12 16:06:28 +0200

    level 1 refactor into components and custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
-import { useState } from 'react';
 import './App.css';
-
-type Service = {
-  label: string;
-  price: number;
-}
+import useTotalHandler from './hooks/useTotalHandler';
+import Services from './components/Services';
 
 function App() {
-  const [services, setServices] = useState<Service[]>([]); // ['web', 'seo', 'ads'
-  const [total, setTotal] = useState(0);
-
-  const totalHandler = (label: string, price: number) => {
-    const service = document.querySelector(label) as HTMLInputElement;
-
-    if (service.checked) {
-      setServices((prev) => [...prev, { label, price }]);
-      setTotal((prev) => prev + price);
-    } else {
-      setServices((prev) => prev.filter((service: Service) => service.label !== label));
-      setTotal((prev) => prev - price);
-    }
-  }
-
-  console.log("services", services);
+  const { total, updateTotal } = useTotalHandler();
 
   return (
     <>
       <h2>¿Qué quieres hacer?</h2>
       <div className="services">
-        <label>
-          <input type="checkbox" name="web" value="web" id="web" className="exclusive-checkbox" onChange={() => totalHandler("#web", 500)} />
-          Una página web (500€)
-        </label>
-        <label>
-          <input type="checkbox" name="seo" value="seo" id="seo" className="exclusive-checkbox" onChange={() => totalHandler("#seo", 300)} />
-          Una consultoría SEO (300€)
-        </label>
-        <label>
-          <input type="checkbox" name="ads" value="ads" id="ads" className="exclusive-checkbox" onChange={() => totalHandler("#ads", 200)} />
-          Una campanya de Google Ads (200€)
-        </label>
-        <div className="total">Total: {total}</div>
+        <Services label={"#web"} price={600} message={"Una página web (600€)"} updateTotal={updateTotal} />
+        <Services label={"#seo"} price={300} message={"Una consultoría SEO (300€)"} updateTotal={updateTotal} />
+        <Services label={"#ads"} price={400} message={"Una campaña de anuncios (400€)"} updateTotal={updateTotal} />
+
+        <div className="total">Total: {total}€</div>
       </div>
     </>
   );
